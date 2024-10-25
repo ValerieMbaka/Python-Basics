aef277c49b71f74cc4cd879c0c509254a89a068a
ValerieMbaka <valeriembaka0@gmail.com>
2024-10-25 16:06:21 +0300

    data types

---
+++ dictionaries.py
@@ -0,0 +1,14 @@
+student = {
+    # key: value
+    "name": "Mark",
+    "year": 2005,
+    "school": "Multimedia University",
+    "mean_score": 80.1
+}
+
+laptop = {
+    "brand": "hp",
+    "RAM": "16GB",
+    "processor": "i7",
+    "processor_speed": "4.6GHz"
+}
--- lists.py
+++ lists.py
@@ -8,6 +8,17 @@ print(len(students))
 # Accessing list items
 print(students[0])
 
+# Print the last item in the tuple
+print(students[-1])
+
+# Printing a range of items in the list
+print(students[:3])  # Prints items up to index 2, the third index is ignored
+
+print(students[1:3]) # Prints items from index 1 to index 2
+print(students[-1:-3])  # Prints nothing because the range is invalid
+print(students[-3:-1])  # Prints items from the third last index to the second last index
+
+
 # Write a program to print out the specific index of the last item in a list
 index = len(students) - 1
 print(index)
@@ -19,7 +30,9 @@ students.insert(1, "Yvonne")
 # append() adds the item to the end of the list
 students.append("Pauline")
 
-
 # Removing items from a list
 # remove()
 students.remove("Hyke")
+
+# Changing the value of an index
+students[0] = "Faith"
