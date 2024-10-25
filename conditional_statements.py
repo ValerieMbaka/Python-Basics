638f9fb572076f606ad19c9f8e7d8f02007a5a60
ValerieMbaka <valeriembaka0@gmail.com>
2024-10-25 16:17:03 +0300

    conditional statements

---
+++ conditional_statements.py
@@ -0,0 +1,12 @@
+# If statements
+math = 72
+english = 56
+
+if math < english:
+    print("Maths is less than English")
+
+elif math > english:
+    print("Maths is greater than English")
+
+else:
+    print("Maths is equal to English")
